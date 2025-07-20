08d27d20510587bcd74850891318aec25b44a83f
takaki <inokutitakaki@yahoo.co.jp>
2025-07-20 14:05:44 +0900

    修正

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const form = document.getElementById("contact-form");
-const confirmation = document.getElementById("confirmation");
+  const form = document.getElementById("contact-form");
+  const confirmation = document.getElementById("confirmation");
 
-form.addEventListener("submit",(e) => {
-  e.prebentDefault();
-  
-  confirmation.style.display = "block";
-  
-  form.reset();
+  form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    
+    confirmation.style.display = "block";
+    
+    form.reset();
 
-});
+  });
