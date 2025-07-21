9786cc6579eb596dcf1032ab427fb696876ca161
takaki <inokutitakaki@yahoo.co.jp>
2025-07-21 17:42:30 +0900

    変更

--- neko-management-app/src/App.js
+++ neko-management-app/src/App.js
@@ -92,23 +92,3 @@ return(
 }
 
 export default App;
-// const form = document.querySelector("#cat-form");//cat-formの情報をformへ
-// const nameInput = document.querySelector("#cat-name");
-
-// form.addEventListener("submit", async(e) =>{
-//   e.preventDefault();
-
-//   const name = nameInput.value;
-
-//   try{
-//     await addDoc(collection(db, "cats"),{
-//       name: name,
-//       createdAt: new Date(),
-//     });
-
-//     alert("猫の情報を登録しました！");
-//     form.reset();
-//   } catch(error){
-//     alert("登録中にエラーが発生しました：" + error);
-//   }
-// });
