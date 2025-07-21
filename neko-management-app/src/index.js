cf606c8224a207786028cd55f950c03e0b43208a
takaki <inokutitakaki@yahoo.co.jp>
2025-07-21 15:35:28 +0900

    css更新

--- neko-management-app/src/index.js
+++ neko-management-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../style/stylecss';
+import '../style/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
