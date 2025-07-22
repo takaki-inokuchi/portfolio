2c8bd7240b7301255986f67833262724f088dead
takaki <inokutitakaki@yahoo.co.jp>
2025-07-22 22:47:21 +0900

    App.js更新

--- src/App.js
+++ src/App.js
@@ -1,89 +1,89 @@
-import React,{useState} from "react";
-import{db} from "./firebase";
-import {collection,addDoc} from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "./firebase";
+import { collection, addDoc } from "firebase/firestore";
 import './style/style.css';
 
-function App(){
+function App() {
   const [catName, setCatName] = useState('');
   const [contact, setContact] = useState({
-    name:'',
-    email:'',
-    message:'',
+    name: '',
+    email: '',
+    message: '',
   });
   const [contactSent, setContactSent] = useState(false);
 
-const handleCatSubmit = async (e) => {
-  e.preventDefault();//ページの動きを止めながらデータを送信する関数
-  try{
-    await addDoc(collection(db, "cats"),{
-      name: catName,
-      createdAt: new Date(),
-    });
-    alert("猫の情報を登録しました！");
-    setCatName('');//入力フォームを空にする
-  } catch(error){
-    alert("登録中にエラーが発生しました：" + error);
-  }
-};
+  const handleCatSubmit = async (e) => {
+    e.preventDefault();//ページの動きを止めながらデータを送信する関数
+    try {
+      await addDoc(collection(db, "cats"), {
+        name: catName,
+        createdAt: new Date(),
+      });
+      alert("猫の情報を登録しました！");
+      setCatName('');//入力フォームを空にする
+    } catch (error) {
+      alert("登録中にエラーが発生しました：" + error);
+    }
+  };
 
-const handleContactChange = (e) =>{//処理が行われたとき
-  setContact({...contact,[e.target.name]: e.target.value});
-};
+  const handleContactChange = (e) => {//処理が行われたとき
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
 
-const handleContactSubmit = (e) =>{
-  e.preventDefault();
+  const handleContactSubmit = (e) => {
+    e.preventDefault();
 
-  setContactSent(true);
-};
+    setContactSent(true);
+  };
 
-return(
-  <div className="App"> 
-    <h1>猫登録アプリ</h1>
-    <p>猫との大切な時間をメモリに残そう！</p>
-    <p>まずは猫の情報を登録してみよう</p>
+  return (
+    <div className="App">
+      <h1>猫登録アプリ</h1>
+      <p>猫との大切な時間をメモリに残そう！</p>
+      <p>まずは猫の情報を登録してみよう</p>
 
-    <form onSubmit={handleCatSubmit}>
-      <label htmlFor="cat-name">猫の名前</label>
-      <input
-        type="text"
-        id="cat-name"
-        value={catName}
-        onChange={(e) => setCatName(e.target.value)}//入力欄に何か実行されたら発動するイベント
-        required//必ず入力して下さいという意味
-      />
-      <button type="submit">登録する</button>
-    </form>
+      <form onSubmit={handleCatSubmit}>
+        <label htmlFor="cat-name">猫の名前</label>
+        <input
+          type="text"
+          id="cat-name"
+          value={catName}
+          onChange={(e) => setCatName(e.target.value)}//入力欄に何か実行されたら発動するイベント
+          required//必ず入力して下さいという意味
+        />
+        <button type="submit">登録する</button>
+      </form>
 
-     <form action="https://formspree.io/f/mzzvdeee" method="POST">
-    <label>名前</label>
-    <input
-      type="text"
-      name="name"
-      value={contact.name}
-      onChange={handleContactChange}
-      required
-    />
-    <label>メールアドレス：</label>
-    <input
-      type="email"
-      name="email"
-      value={contact.email}
-      onChange={handleContactChange}
-      required
-    />
-    <label>メッセージ</label>
-    <textarea
-      name="message"
-      value={contact.message}
-      onChange={handleContactChange}
-      required
-    ></textarea>
+      <form action="https://formspree.io/f/mzzvdeee" method="POST">
+        <label>名前</label>
+        <input
+          type="text"
+          name="name"
+          value={contact.name}
+          onChange={handleContactChange}
+          required
+        />
+        <label>メールアドレス：</label>
+        <input
+          type="email"
+          name="email"
+          value={contact.email}
+          onChange={handleContactChange}
+          required
+        />
+        <label>メッセージ</label>
+        <textarea
+          name="message"
+          value={contact.message}
+          onChange={handleContactChange}
+          required
+        ></textarea>
 
-    <button type="submit">送信</button>
-    {contactSent && <p>送信が完了しました！</p>}
-  </form>
-  </div>
-);
+        <button type="submit">送信</button>
+        {contactSent && <p>送信が完了しました！</p>}
+      </form>
+    </div>
+  );
 }
 
 export default App;
