5f163c0b4989de16557e8968d30be1ede4da197b
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-30 19:32:48 +0900

    clone

--- src/app/componets/Footer.tsx
+++ src/app/componets/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <div className="flex items-center justify-center h-12 bg-orange-300 text-white">
-      @2025 takaki inokuchi
+      @2025 takaki inokuchi 
     </div>
   )
 }
