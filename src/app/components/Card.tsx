3bcf30518e16800f6d0c53cf94c54c7dcad6ac89
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-31 19:42:30 +0900

    真ん中

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,7 +3,7 @@ import { WorkCardProps } from "../data/data";
 
 const Card = (props: WorkCardProps) => {
   return (
-    <div className="p-2 w-110 h-100">
+    <div className="p-2 w-100 h-80">
       <a href={`${props.link}?title=${props.title}`}>
         <Image src={props.image} alt={props.title} width={400} height={400} className="object-cover w-100 h-80 rounded mx-auto"/>
       </a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <p className="flex justify-center py-15">
           未経験からWeb開発を学習中。Reactを使用し開発アプリを制作しています！
         </p>
-        <div className=" grid grid-cols-1 p-2 sm:grid-cols-2 place-items-center">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 place-items-center">
           {works.map((work) => (
             <Card
               key={work.title}
