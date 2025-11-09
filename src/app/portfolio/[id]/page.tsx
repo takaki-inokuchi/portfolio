0811c520ea17bb344e004285bba40338cd5553cf
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-11-09 14:26:58 +0900

    width Adjustment

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -10,7 +10,7 @@ const PortfolioDetail = async ({ params }: PortfolioDetailProps) => {
 
   return (
     <div className="max-w-3xl mx-auto">
-      <h1 className="py-14 text-6xl font-bold max-w-full">{item?.title}</h1>
+      <h1 className="py-14 text-6xl font-bold break-words max-w-full">{item?.title}</h1>
 
       <Tag tags={item?.tags ?? []} />
 
