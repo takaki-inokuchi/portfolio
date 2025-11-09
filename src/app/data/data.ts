2298802ed44116f1e576fe9734efa6f3aecda630
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-11-09 14:24:11 +0900

    width Adjustment

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -39,41 +39,23 @@ export const portfolioData: PortfolioItem[] = [
       }
     ]
   },
-    {
+  {
     id: "indiegamesite",
-    title: "Indie",
+    title: "IndieGameSite",
     image: "/IndieSite.png",
-    tags: ["Webアプリ", "AIアプリ", "toC"],
-    url: "https://cakemate-orcin.vercel.app/",
-    outline: "このアプリでは、AIを活用して、ユーザーがケーキのデザインを簡単に作成できるように支援します。ユーザーは、好みのスタイルやテーマを選択し、AIがそれに基づいて独自のケーキデザインを生成します。これにより、特別なイベントやお祝いのために、個性的で魅力的なケーキを手軽に作成できます。",
+    tags: ["Webアプリ", "toC", "コミュニティ", "ゲームシェア"],
+    url: "https://indiegame-hub.web.app/",
+    outline: " このアプリでは、個人製作されたゲームの一覧を確認できます。最新ゲームやランキングを閲覧し、ゲーム情報を共有することが可能です。マイナーなゲームも気軽に共有できる場を提供しています。",
     skillData: [
       {
         language: "javascript , TypeScript",
         role: "Frontend Development , UI Design , UX Design",
         client: "個人開発",
-        period: "2025/09~2025/10",
+        period: "2025/10~2025/11",
         technology:
-          "TypeScript , React ,Jest ",
+          "TypeScript , React , supabase , Chakra UI , GitHub Actions , Jest",
       }
-    ]
-  },
-  // {
-  //   id: "indiegamesite",
-  //   title: "IndieGameSite",
-  //   image: "/IndieSite.png",
-  //   tags: ["Webアプリ", "toC", "コミュニティ", "ゲームシェア"],
-  //   url: "https://indiegame-hub.web.app/",
-  //   outline: " このアプリでは、個人製作されたゲームの一覧を確認できます。最新ゲームやランキングを閲覧し、ゲーム情報を共有することが可能です。マイナーなゲームも気軽に共有できる場を提供しています。",
-  //   skillData: [
-  //     {
-  //       language: "javascript , TypeScript",
-  //       role: "Frontend Development , UI Design , UX Design",
-  //       client: "個人開発",
-  //       period: "2025/10~2025/11",
-  //       technology:
-  //         "TypeScript , React , supabase , Chakra UI , GitHub Actions , Jest",
-  //     }
-  //   ],
+    ],
 
-  // },
+  },
 ];
